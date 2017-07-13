f7834a28bff391d5789db1dd9e4f1313ee7751ba
Mohammad <mohammad.altahan@sharedop.com>
2017-07-13 15:01:22 +0200

    test merging

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,13 +1,22 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace FirstIotProject
+namespace App1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -17,15 +26,7 @@ namespace FirstIotProject
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void ClickMe_Click(object sender, RoutedEventArgs e)
-        {
-            
-            this.HelloMessage.Text = "Hello, Windows 10 IoT Core2!";
             ShutdownManager.BeginShutdown(ShutdownKind.Restart, TimeSpan.FromSeconds(5));
         }
-
-
     }
 }
--- FirstIotProject/DigitalAcuator.cs
+++ FirstIotProject/DigitalAcuator.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.ApplicationModel.Contacts.Provider;
-
+﻿
 namespace FirstIotProject
 {
     class DigitalAcuator
     {
 
-     
+        void BindToRunningProcesses()
+        {
+            // this is the Iot branche
+        }
     }
 }
